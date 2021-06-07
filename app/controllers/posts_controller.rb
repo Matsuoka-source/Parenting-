07d5e9d5475a227a8d4ae0b82e614fb13baab5b1
Matsuoka-source <tm19930425@gmail.com>
2021-06-07 08:56:01 +0000

    Post

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,41 @@
+class PostsController < ApplicationController
+  def index
+    @post = Post.new
+    # 新しい投稿
+    @posts = Post.all
+    # すべての投稿
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    # ユーザーと投稿を紐づけ
+    @post.save
+    # 情報を保存
+    redirect_to posts_path
+
+  end
+
+  def edit
+    @post = Post.new
+    # 新規作成用
+  end
+
+  def show
+    @post = Post.find(params[:id])
+    # 主キーに対応するレコードを取り出す
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    # 主キーに対応するレコードを取り出す
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  private
+  # 投稿データのストロングパラメータ
+  def post_params
+    params.require(:post).permit(:user_id, :title, :image, :body)
+  end
+end
---
+++ app/models/post.rb
@@ -0,0 +1,9 @@
+class Post < ApplicationRecord
+  belongs_to :user
+  # has_many :post_comments, dependent: :destroy
+  # コメント機能との関係
+  # has_many :favorites, dependent: :destroy
+  # お気に入り機能との関係
+  attachment :image
+  # 画像アップ用のメソッド追加（refileのルール）
+end
