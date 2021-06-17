16f8ba5b9fd33c67f52e7953acfa02f1c61e0948
Matsuoka-source <tm19930425@gmail.com>
2021-06-17 06:33:36 +0000

    search2

--- app/controllers/searchs_controller.rb
+++ app/controllers/searchs_controller.rb
@@ -1,19 +1,17 @@
 class SearchsController < ApplicationController
   def search
-    # viewのform_tagにて
-    # 選択したmodelの値を@modelに代入。
+    # viewのform_tagで選択したmodelの値を@modelに代入。
     @model = params["model"]
     # 選択した検索方法の値を@methodに代入。
     @method = params["method"]
     # 検索ワードを@contentに代入。
     @content = params["content"]
-    # @model, @content, @methodを代入した、
-    # search_forを@recordsに代入。
-    @records = search_for(@model, @content, @method)
+    # @model, @content, @methodを代入したsearch_forを@recordsに代入。
+    @records = search_for(@model, @method, @content)
   end
 
   private
-  def search_for(model, content, method)
+  def search_for(model, method, content)
     # 選択したモデルがuserだったら
     if model == 'user'
       # 選択した検索方法がが完全一致だったら
