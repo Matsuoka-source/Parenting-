512becc27e99ced8a58fbcb1ae78337eea1e0bf9
Matsuoka-source <tm19930425@gmail.com>
2021-07-06 08:13:07 +0000

    post current_user Fix

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,8 @@
 class PostsController < ApplicationController
+
+  before_action :authenticate_user!
+  before_action :ensure_correct_user, only: [:edit, :update, :destroy]
+
   def index
     @post = Post.new
     # 新しい投稿
@@ -31,12 +35,18 @@ class PostsController < ApplicationController
   end
 
   def update
-    post = Post.find(params[:id])
-    post.update(post_params)
-    if post.user == current_user
-      redirect_to user_path(current_user.id)
+    # post = Post.find(params[:id])
+    # post.update(post_params)
+    # if post.user == current_user
+    #   redirect_to user_path(current_user.id)
+    # else
+    #   render "index"
+    # end
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to post_path(@book), notice: "You have updated book successfully."
     else
-      render "index"
+      render "edit"
     end
   end
 
@@ -60,5 +70,12 @@ class PostsController < ApplicationController
   # 投稿データのストロングパラメータ
   def post_params
     params.require(:post).permit(:user_id, :title, :image, :body)
+  end
+
+  def ensure_correct_user
+    @post = Post.find(params[:id])
+    unless @post.user == current_user
+      redirect_to posts_path
+    end
   end
 end
